1e8810d72061743d7fa1ae4ec5468cedf2ab182b
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-09-08 23:25:06 -0300

    feat: remove versioning from mongo

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 
-const livroSchema = new mongoose.Schema({
-    id: { type: String },
-    titulo: { type: String, required: true },
-    autor: { type: String, required: true },
-    editora: { type: String, required: true },
-    numeroDePaginas: { type: Number },
-});
+const livroSchema = new mongoose.Schema(
+    {
+        id: { type: String },
+        titulo: { type: String, required: true },
+        autor: { type: String, required: true },
+        editora: { type: String, required: true },
+        numeroDePaginas: { type: Number },
+    },
+    {
+        versionKey: false,
+    }
+);
 
 const livros = mongoose.model("livros", livroSchema);
 

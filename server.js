bae5e314f86d3ced596fe92466e28b2e48ad7a83
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-09-09 19:51:35 -0300

    bug: heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import app from "./src/app.js";
 
 const port = process.env.PORT || 3000;
 
+console.log(process.env.PORT);
+
 app.listen(port, () => {
     console.log(`Listening in http://localhost:${port}`);
 });

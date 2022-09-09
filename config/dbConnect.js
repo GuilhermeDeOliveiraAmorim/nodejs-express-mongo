47c64ea08c7dc23229c73422cbf9f00aa0c8f4fd
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-09-09 20:27:15 -0300

    bug: try

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 
 const urlConnection =
-    "mongodb+srv://guilhermeamorim:rJTUXt7iKn5rr!6@cluster0.5zfqrm3.mongodb.net/alura-node";
-// "mongodb+srv://" +
-// process.env.DB_HOST +
-// ":" +
-// process.env.SECRET_KEY +
-// "@" +
-// process.env.DB_CLUSTER +
-// "/" +
-// process.env.DB_DATA;
+    // "mongodb+srv://guilhermeamorim:rJTUXt7iKn5rr!6@cluster0.5zfqrm3.mongodb.net/alura-node";
+    "mongodb+srv://" +
+    process.env.DB_HOST +
+    ":" +
+    process.env.SECRET_KEY +
+    "@" +
+    process.env.DB_CLUSTER +
+    "/" +
+    process.env.DB_DATA;
 
 mongoose.connect(urlConnection);
 

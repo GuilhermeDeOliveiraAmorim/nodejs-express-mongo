d36acf766eeb7632fd4a387a2ff19b2dce3643f8
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-09-09 22:51:10 -0300

    feat: add unique for login

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
     {
         id: { type: String },
-        login: { type: String, required: true },
+        login: { type: String, required: true, unique: true },
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
     },
